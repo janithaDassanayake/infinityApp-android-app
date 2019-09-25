70e635db5bff24bf0c7ede76b525f64b2c4d0138
pramodya97 <pramodyahettiarachchi@gmail.com>
2019-09-25 11:51:50 +0530

    SelectMovie, SelectDateTime Interface, drawable error corrected/ add new activity

--- app/src/main/java/com/example/infinityapp/SelectDateTime.java
+++ app/src/main/java/com/example/infinityapp/SelectDateTime.java
@@ -2,20 +2,40 @@ package com.example.infinityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SelectDateTime extends AppCompatActivity {
+
+    Button nextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_date_time);
 
+        nextBtn = findViewById(R.id.testbtn);
+
         TextView textView = (TextView)findViewById(R.id.txt_bundle);
         Bundle bundle = getIntent().getExtras();
         String data = bundle.get("data").toString();
         textView.setText(data);
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SelectDateTime.this, SelectTheaterSeats.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/infinityapp/SelectTheaterSeats.java
@@ -0,0 +1,19 @@
+package com.example.infinityapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class SelectTheaterSeats extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_theater_seats);
+
+
+    }
+}
